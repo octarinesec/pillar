6b1d496b9a40980b193258f508fa492d4da2e9e3
Marco Priebe <marco.priebe@inoio.de>
2016-03-30 13:21:22 +0200

    upgrade scala version and sbt-sonatype plugin

--- project/PillarBuild.scala
+++ project/PillarBuild.scala
@@ -68,8 +68,8 @@ object PillarBuild extends Build {
     version := "2.1.0",
     homepage := Some(url("https://github.com/Galeria-Kaufhof/pillar")),
     licenses := Seq("MIT license" -> url("http://www.opensource.org/licenses/mit-license.php")),
-    scalaVersion := "2.10.4",
-    crossScalaVersions := Seq("2.10.4", "2.11.1"),
+    scalaVersion := "2.11.6",
+    crossScalaVersions := Seq("2.10.4", "2.11.6"),
     rhPackageTask
   ).settings(
     publishTo := {
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,6 +2,4 @@ addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
 
-addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.2.1")
-
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "1.1")
